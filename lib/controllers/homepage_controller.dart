6a43f6a7378799978ce3949570c057fcf2174cc0
numanarif990 <nomigjr990@icloud.com>
2024-12-29 09:50:02 +0500

    column has been replaced by list view builder

---
+++ lib/controllers/homepage_controller.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/cupertino.dart';
+import 'package:get/get.dart';
+
+import '../pages/home_page_containers/container1/container1.dart';
+import '../pages/home_page_containers/container2/container2.dart';
+import '../pages/home_page_containers/container3/container3.dart';
+import '../pages/home_page_containers/container4/container4.dart';
+import '../utils/colors.dart';
+import '../widgets/bottom/bottom.dart';
+import '../widgets/navbar/navbar.dart';
+
+class HomepageController extends GetxController {
+  final ScrollController scrollController = ScrollController();
+  final widgets = <Widget>[].obs;
+  final isLoadingMore = false.obs;
+  final allDataLoaded = false.obs;
+
+  @override
+  void onInit() {
+    super.onInit();
+    scrollController.addListener(_onScroll);
+    _loadInitialData();
+  }
+
+  Future<void> _loadInitialData() async {
+    await Future.delayed(const Duration(seconds: 2));
+    widgets.addAll([
+      const Navbar(),
+      const Container1(),
+      _buildDivider(Get.size),
+      const Container2(),
+      _buildDivider(Get.size),
+      const Container3(),
+      _buildDivider(Get.size),
+      const Container4(),
+      _buildDivider(Get.size),
+      Bottom(),
+    ]);
+  }
+
+  void _onScroll() {
+    if (scrollController.position.pixels == scrollController.position.maxScrollExtent && !isLoadingMore.value && !allDataLoaded.value) {
+      _loadMoreData();
+    }
+  }
+
+  Future<void> _loadMoreData() async {
+    isLoadingMore.value = true;
+
+    // Simulate loading additional data
+    await Future.delayed(const Duration(seconds: 2));
+
+    isLoadingMore.value = false;
+    allDataLoaded.value = true; // Assuming no more data to load
+  }
+
+  @override
+  void onClose() {
+    scrollController.dispose();
+    super.onClose();
+  }
+
+  static Widget _buildDivider(Size size) {
+    return Container(
+      width: size.width,
+      height: 0.2,
+      color: AppColors.buttonColor,
+    );
+  }
+}
--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,51 +1,34 @@
 import 'package:flutter/material.dart';
-import 'package:hexham_pint/pages/home_page_containers/container1/container1.dart';
-import 'package:hexham_pint/pages/home_page_containers/container2/container2.dart';
-import 'package:hexham_pint/pages/home_page_containers/container3/container3.dart';
-import 'package:hexham_pint/pages/home_page_containers/container4/container4.dart';
-import 'package:hexham_pint/utils/colors.dart';
-import 'package:hexham_pint/widgets/bottom/bottom.dart';
-import 'package:hexham_pint/widgets/navbar/navbar.dart';
+import 'package:get/get.dart';
+
+
+import '../controllers/homepage_controller.dart';
 class Homepage extends StatelessWidget {
   const Homepage({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    Size size = MediaQuery.of(context).size;
-    return Scaffold(
-      backgroundColor: Colors.white,
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-           const Navbar(),
-            const Container1(),
-            Container(
-              width: size.width,
-              height: 0.2,
-              color: AppColors.buttonColor,
-            ),
-            const Container2(),
-            Container(
-              width: size.width,
-              height: 0.2,
-              color: AppColors.buttonColor,
-            ),
-            const Container3(),
-            Container(
-              width: size.width,
-              height: 0.2,
-              color: AppColors.buttonColor,
-            ),
-            const Container4(),
-            Container(
-              width: size.width,
-              height: 0.2,
-              color: AppColors.buttonColor,
-            ),
-            Bottom()
-          ],
-        ),
-      ),
-    );
-  }
+    Widget build(BuildContext context) {
+      final HomepageController controller = Get.put(HomepageController());
+
+      return Scaffold(
+        backgroundColor: Colors.white,
+        body: Obx(() {
+          if (controller.widgets.isEmpty) {
+            return const Center(child: CircularProgressIndicator());
+          }
+
+          return ListView.builder(
+            controller: controller.scrollController,
+            itemCount: controller.widgets.length + (controller.isLoadingMore.value ? 1 : 0),
+            itemBuilder: (context, index) {
+              if (index < controller.widgets.length) {
+                return controller.widgets[index];
+              } else {
+                return const Center(child: CircularProgressIndicator());
+              }
+            },
+          );
+        }),
+      );
+    }
 }
